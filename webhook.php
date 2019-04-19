54c1dbe7418a1d2d3444816a8815e4374d26ec11
DatDraggy <bmxstreet@gmx.net>
2019-04-19 19:45:44 +0200

    debugging

--- webhook.php
+++ webhook.php
@@ -11,7 +11,7 @@ if (isset($data['inline_query'])) {
   $senderUserId = $data['inline_query']['from']['id'];
   $search = $data['inline_query']['query'];
   $newOffset = $data['inline_query']['offset'];
-
+mail($config['mail'], 'Debug', $dump);
   if ($newOffset === '' || $newOffset === 0) {
     $offset = 0;
   } /*elseif (is_numeric($oldOffset)) {
